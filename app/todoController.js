ad65b99001f0f75f49364af7cd4ebab191305659
Abhijit Singh <singh.abhijit66@gmail.com>
2015-05-26 00:24:13 +0530

    wip for todo app

--- app/todoController.js
+++ app/todoController.js
@@ -4,5 +4,20 @@
 
 angular.module('todoApp.Controller', [])
     .controller('todoController', ["$scope", function($scope) {
-        $scope.newTask = ""
+        $scope.newTask = "";
+
+        $scope.taskList = [
+         {description : "task 1", done : false},
+         {description : "task 2", done : false},
+         {description : "task 3", done : false}
+        ];
+
+        $scope.addTodo = function() {
+            $scope.taskList.push({description: $scope.newTask, done:false})
+            $scope.newTask=""
+        };
+
+        $scope.deleteTodo = function(index) {
+            $scope.taskList.splice(index,1);
+        }
     }]);
